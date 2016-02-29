0eaef310c040393095ab7757a6363a4d2fd6a844
Andrew Lobban <andrewglobban@gmail.com>
2016-02-29 07:03:01 -0600

    Initial commit of MEAN project

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+
+/**
+* Express App
+* @author Andrew Lobban <andrewglobban@gmail.com>
+**/
+
+
+/** require modules **/
+
+var express = require("express");
+
+/** Initialize Express app object **/
+
+var app = express();
