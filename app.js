9d9644c726be8c765fe840803b1d2e42ca06bb43
Andrew Lobban <andrewglobban@gmail.com>
2016-02-29 20:18:03 -0600

    Add Express Server configuration

--- app.js
+++ app.js
@@ -14,3 +14,21 @@ var express = require("express");
 /** Initialize Express app object **/
 
 var app = express();
+
+/** Vaiables **/
+
+/** Server will be browsed at http://localhost:3000 **/
+
+var root = __dirname,
+	port = 3000;
+
+/** configure Express app **/
+
+app.use( express.static( root + "/public" ));
+
+/** start server on port 3000 **/
+
+app.listen( port, function listenCallback () {
+	console.log( "Express server is listening on port " + port );
+	console.log( "To test, browse to http://localhost:" + port );
+})
