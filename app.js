dc66522c4222558b75ce58e5fdaca7c12f3a238b
Andrew Lobban <andrewglobban@gmail.com>
2016-03-05 16:20:23 -0600

    Install body-parser

--- app.js
+++ app.js
@@ -10,7 +10,9 @@
 /** require modules **/
 
 var express = require("express"),
-	jade = require("jade");
+	jade = require("jade"),
+	connect = require("connect"),
+	bodyParser = require("body-parser");
 
 /** Initialize Express app object **/
 
@@ -31,6 +33,10 @@ app.set( "views", root + "/views" );
 
 app.set( "view engine", "jade" );
 
+/** Add middleware that will look for data when requests are made **/
+
+app.use( bodyParser.urlencoded( {extended: true} ));
+
 /** Create some Express routes **/
 
 app.get( "/settings/profile", function editProfileCb (req, res) {
@@ -41,11 +47,23 @@ app.get( "/settings/profile", function editProfileCb (req, res) {
 
 app.post("/settings/profile", function postProfileCb (req, res) {
 
+	// this callback is fired after a POST request is sent to the server
+
+	// report to console
+
 	console.log( "POST RECEIVED!" );
 
-	res.json({
-		"status": "Post received!"
-	})
+	// report post data to console
+
+	console.log( req.body );
+
+	// reply to browser that something has happened and close the loop
+
+	return res.json({
+		"firstName": req.body.firstNameField,
+		"lastName": req.body.lastNameField,
+		"bio": req.body.bioField
+	});
 });
 
 app.get( "/someJSON", function someJSONCallback (req, res) {
